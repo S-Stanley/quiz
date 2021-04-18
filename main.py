fa8bcf1b16c9f6c1320eb7a10a53826ef00d65c9
Stanley SERBIN <stanleyserbin@gmail.com>
2021-04-18 16:35:56 -0400

    UPDATE set total as global

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from physique import Physique
 from math import Math
 
+__TOTAL__ = 3
+
 def main():
 	score = 0
-	total = 0
-	
+
 	score += Physique.planetes()
-	total += 1
-
 	score += Math.nombre_premier_lvl_1()
-	total += 1
-
 	score += Math.nombre_premier_lvl_2()
-	total += 1
 
-	print(f'Final Grade : {int(score/total) * 20}/20')
+	global __TOTAL__
+	print(f'Final Grade : {int((score/__TOTAL__)* 20)}/20')
 
 main()
