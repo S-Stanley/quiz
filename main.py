fe501fc2959329ec407d9c6dd5db31a535426242
Stanley SERBIN <stanleyserbin@gmail.com>
2021-04-18 16:46:12 -0400

    UPDATE add random question

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import random
 from physique import Physique
-from math import Math
+from mathematics import Math
 
 __TOTAL__ = 3
 
 def main():
 	score = 0
-
-	score += Physique.planetes()
-	score += Math.nombre_premier_lvl_1()
-	score += Math.nombre_premier_lvl_2()
-
+	question = []
+	question.extend([ Physique.planetes, Math.nombre_premier_lvl_1, Math.nombre_premier_lvl_2])
+	random.shuffle(question)
+	for key in question:
+		score += key()
 	global __TOTAL__
 	print(f'Final Grade : {int((score/__TOTAL__)* 20)}/20')
 
